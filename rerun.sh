35e60f080bd54f334878e52e27b14f374b22e3dc
Jack Weissburg <jweissburg@pivotal.io>
2019-08-14 14:05:38 -0700

    Pipeline yml and start text

--- rerun.sh
+++ rerun.sh
@@ -1,4 +1,9 @@
-#set -e
 cf uninstall-plugin pcc
 go build main.go helpers.go strings.go structs.go formatting.go argument_parser.go commandMapping.go
 echo Y | cf install-plugin main
+
+echo "    _______________  ______________________"
+echo "   / _____/ ______/ / ____  / _____/ _____/"
+echo "  / /    / /___    / /___/ / /    / /    "
+echo " / /____/ ____/   / ______/ /____/ /____"
+echo "/______/_/       /_/     /______/______/ cf pcc 1.0.0"

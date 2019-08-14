3c8266ec64b2dd375ea0dcac50aa9c3abd8f1063
Jack Weissburg <jweissburg@pivotal.io>
2019-08-14 14:37:27 -0700

    Pipeline built

--- rerun.sh
+++ rerun.sh
@@ -2,8 +2,8 @@ cf uninstall-plugin pcc
 go build main.go helpers.go strings.go structs.go formatting.go argument_parser.go commandMapping.go
 echo Y | cf install-plugin main
 
-echo "    _______________  ______________________"
-echo "   / _____/ ______/ / ____  / _____/ _____/"
-echo "  / /    / /___    / /___/ / /    / /    "
-echo " / /____/ ____/   / ______/ /____/ /____"
-echo "/______/_/       /_/     /______/______/ cf pcc 1.0.0"
+echo "    _______  ________    ________  _______  _______"
+echo "   / _____/ / ______/   / ____  / / _____/ / _____/"
+echo "  / /      / /___      / /___/ / / /      / /    "
+echo " / /____  / ____/     / ______/ / /____  / /____"
+echo "/______/ /_/         /_/       /______/ /______/ 1.0.0"

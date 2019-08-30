51722289e987c4ef2700c8ea7de78577d904708b
Joris Melchior <joris.melchior@gmail.com>
2019-08-30 11:39:13 -0400

    Remove binaries and checksum files from repo
    Improve the build script for binaries
    
    Authored-by: Joris Melchior <joris.melchior@gmail.com>

--- releases/generateBinaries.sh
+++ releases/generateBinaries.sh
@@ -1,9 +1,25 @@
-cd linux64
+#!/usr/bin/env bash
+
+if [ ! -d "./linux64" ]; then
+  mkdir ./linux64
+fi
+cd linux64 || exit 1
 env GOOS=linux GOARCH=amd64 go build -o ./pcc ../../cmd/main
 md5 ./pcc > md5.txt
-cd ../osx
+echo "Built linux executable"
+
+if [ ! -d "../osx" ]; then
+  mkdir ../osx
+fi
+cd ../osx || exit 1
 env GOOS=darwin GOARCH=amd64 go build -o ./pcc ../../cmd/main
 md5 ./pcc > md5.txt
-cd ../win64
+echo "Built darwin executable"
+
+if [ ! -d "../win64" ]; then
+  mkdir ../win64
+fi
+cd ../win64 || exit 1
 env GOOS=windows GOARCH=amd64 go build -o ./pcc.exe ../../cmd/main
 md5 ./pcc.exe > md5.txt
+echo "Built windows exectable"

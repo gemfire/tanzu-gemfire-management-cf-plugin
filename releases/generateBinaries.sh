97c689649e90ab68378ab145e55def84fad8bf13
Jinmei Liao <jiliao@pivotal.io>
2020-03-26 14:59:38 -0700

    rename pcc to gemfire

--- releases/generateBinaries.sh
+++ releases/generateBinaries.sh
@@ -21,22 +21,22 @@ if [ ! -d "./linux64" ]; then
   mkdir ./linux64
 fi
 cd linux64 || exit 1
-env GOOS=linux GOARCH=amd64 go build -o ./pcc ../../cmd/main
-md5 ./pcc >md5.txt
+env GOOS=linux GOARCH=amd64 go build -o ./gemfire ../../cmd/main
+md5 ./gemfire >md5.txt
 echo "Built linux executable"
 
 if [ ! -d "../osx" ]; then
   mkdir ../osx
 fi
 cd ../osx || exit 1
-env GOOS=darwin GOARCH=amd64 go build -o ./pcc ../../cmd/main
-md5 ./pcc >md5.txt
+env GOOS=darwin GOARCH=amd64 go build -o ./gemfire ../../cmd/main
+md5 ./gemfire >md5.txt
 echo "Built darwin executable"
 
 if [ ! -d "../win64" ]; then
   mkdir ../win64
 fi
 cd ../win64 || exit 1
-env GOOS=windows GOARCH=amd64 go build -o ./pcc.exe ../../cmd/main
-md5 ./pcc.exe >md5.txt
+env GOOS=windows GOARCH=amd64 go build -o ./gemfire.exe ../../cmd/main
+md5 ./gemfire.exe >md5.txt
 echo "Built windows executable"
